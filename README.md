5d0170c928bb31033a14222ff866563cd2bea5f7
Paweł Drożański <pawdro@gmail.com>
2023-11-21 16:59:23 +0100

    Naming refactoring

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# nodejs-advanced
+# Node.js (fairly) advanced techniques
+
+1. Event Loop
+   1.1 [explanation](./1-event-loop.js)
+   1.2 [operations - threads](./2-threads.js)
+   1.3 [operations - requests](./3-async.js)
+   1.4 [combined all + fs](./4-multitask.js)
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+function doWork(duration) {
+  const start = Date.now();
+
+  while(Date.now() - start < duration) {}
+}
+
+app.get('/', (_req, res) => {
+  doWork(5000);
+  res.send('Hi there!')
+})
+
+app.listen(3000);
