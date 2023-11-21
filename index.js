238f1ed22cc382d3de9450c2456c599d7979d0ad
Paweł Drożański <pawdro@gmail.com>
2023-11-21 17:51:19 +0100

    Clustering

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
+const cluster = require('cluster')
 const express = require('express')
 const app = express()
 
-function doWork(duration) {
-  const start = Date.now();
+// Is the file being executed in the master mode?
+if (cluster.isMaster) {
+  // Cause index.js to be executed *again* but in the child mode
+  cluster.fork()
+} else {
+  // i'm a child, i'm going to act like a server and do nothing else
+  function doWork(duration) {
+    const start = Date.now();
 
-  while(Date.now() - start < duration) {}
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get('/', (_req, res) => {
+    doWork(5000); // <- blocking operation
+    res.send('Hi there!')
+  })
+
+  app.listen(3000);
 }
 
-app.get('/', (_req, res) => {
-  doWork(5000);
-  res.send('Hi there!')
-})
-
-app.listen(3000);
